ebc0a4e78fc8f3a904ccc15a9ba88a6d84c40aa0
mafia-0001 <sambhavs538@gmail.com>
2026-08-06 00:24:03 +0530

    Add files via upload

---
+++ day 1/app.js
@@ -0,0 +1,16 @@
+// const { log } = require('console');
+// const fs = require('fs');
+// const data = fs.readFileSync('notes.txt','utf-8');
+// fs.writeFileSync('notes.txt', 'vaste gun huyinyaa');
+
+// const path = require('path');
+// const filePath = path.join(__dirname,'test', 'hello.txt');
+// console.log(filePath);
+
+// const { log } = require('console');
+// const os = require('os');
+// const user = os.userInfo();
+// console.log(user);
+
+const des = require("./data/pakages");
+console.log(des);
---
+++ day 1/data/des.js
@@ -0,0 +1,23 @@
+// const des = [
+//     "Goa",
+//     "kerala",
+//     "Rajasthan",
+//     "Haryana"
+// ];
+
+// module.exports = des;
+
+
+// const packages = [
+//     {
+//         name: "package1",
+//         version: "86997789769"
+//     },
+
+//     {
+//         name: "package2",
+//         version: "24514621"
+
+//     }
+// ]
+// module.exports= packages;
