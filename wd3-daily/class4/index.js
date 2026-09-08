5b2831492c87beaff90531d2fef97ccaeca81f9d
mafia-0001 <sambhavs538@gmail.com>
2026-09-08 13:29:53 +0530

    (:

--- wd3-daily/class4/index.js
+++ wd3-daily/class4/index.js
@@ -46,7 +46,7 @@ const server = http.createServer((req, res)=>{
         res.writeHead(200, {'Content-Type': 'text/html'}),
         res.write('<h1>hello world</h1>'),
         res.write('<p>This is my first server</p>'),
-        res.write('<p>My name is Krish</p>'),
+        res.write('<p>My name is Harvey Spectre</p>'),
         res.end()
     }
 });
